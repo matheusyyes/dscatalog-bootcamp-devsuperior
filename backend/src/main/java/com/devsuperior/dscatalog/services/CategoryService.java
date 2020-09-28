b41cec0dddca1ea4368cd1c41806e48302193c1c
Miranda <matheus.300miranda@live.com>
2020-09-28 20:29:35 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -37,5 +37,16 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 		
 	}
+
+	@Transactional()
+	public CategoryDTO insert(CategoryDTO dto) {
+
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		
+		entity = repo.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 	
 }
